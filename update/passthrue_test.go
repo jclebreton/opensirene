6ad604a47de7b4e81e8c6f3a8ec3443f404f8361
JC Lebreton <jclebreton@gmail.com>
2017-09-24 11:26:05 +0200

    Add passthrue test

---
+++ update/passthrue_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Passthrue_Read(t *testing.T) {
+	//Download temp file
+	file := zipFile{
+		filename: "1Mio.dat",
+		url:      "http://ovh.net/files/1Mio.dat",
+		path:     "/tmp/1Mio.dat",
+	}
+	progressChan := make(chan map[string]float64, buffer)
+	errorsChan := make(chan error, buffer)
+	err := downloadZipFile(file, progressChan, errorsChan)
+	assert.NoError(t, err)
+
+	//Open it
+	r, err := os.Open(file.path)
+	assert.NoError(t, err)
+	defer r.Close()
+
+	//Read it
+	progressChan = make(chan map[string]float64, buffer)
+	pt := PassThru{
+		Reader:   r,
+		filename: "foo",
+		progress: progressChan,
+	}
+	p := []byte{1, 12, 2}
+	read, err := pt.Read(p)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 3, read)
+
+	//Progress
+	progress := <-progressChan
+	assert.True(t, progress["foo"] > 0)
+}
