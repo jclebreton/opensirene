9eae42fdfcc3235432d51c34db07697e40b43f17
Jean-Christophe Lebreton <jean-christophe.lebreton@schibsted.com>
2017-11-07 16:38:32 +0100

    Build Debian package with travis

---
+++ scripts/build_deb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BASE_PATH=$(git rev-parse --show-toplevel)
+
+cd $BASE_PATH
+
+latest_tag=$(git describe --abbrev=0)
+commits_since=$(git rev-list --count HEAD "^${latest_tag}")
+
+if git describe --exact-match >/dev/null 2>&1; then
+    deb_version=${latest_tag}
+else
+    # Increment tag and add a ~dev
+    major="${latest_tag%.*}"
+    patch="${latest_tag##*.}"
+    deb_version=${major}.$((patch+1))~dev-${commits_since}
+fi
+
+go build
+
+docker build -t fpm -f fpm.Dockerfile .
+docker run -ti -v $PWD:/packaging fpm fpm --verbose -s dir -t deb -n opensirene -v $deb_version \
+  --description "French company database based on French government open data" \
+  opensirene=/usr/bin/opensirene \
+  conf-example.yml=/usr/share/opensirene/conf-example.yml
