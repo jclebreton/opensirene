4c4df17c18ba555f0243b35329ba25a6d5bf39dc
Matthieu Jacquot <matthieu.jacquot@schibsted.com>
2017-11-10 17:08:53 +0100

    add tests to GetHistories usecase

---
+++ testHelpers/compare.go
@@ -0,0 +1,27 @@
+package testHelpers
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/jclebreton/opensirene/domain"
+)
+
+func CompareHistorySlices(sl1, sl2 []domain.History) error {
+	if len(sl1) != len(sl2) {
+		return errors.New("slices have != lengths")
+	}
+	for _, v1 := range sl1 {
+		found := false
+		for _, v2 := range sl2 {
+			if v2 == v1 {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return fmt.Errorf("Offer %v from slice 1 hasn't been found in slice 2", v1)
+		}
+	}
+	return nil
+}
--- usecases/siren.go
+++ usecases/siren.go
@@ -1,11 +1,45 @@
 package usecases
 
-import "github.com/jclebreton/opensirene/domain"
+import (
+	"github.com/jclebreton/opensirene/domain"
+	"github.com/pkg/errors"
+)
 
-func (i Interactor) GetHistories() ([]domain.History, error) {
+type GetHistoriesRequest struct {
+	// could contain anything useful in order to process the complete usecase
+}
+
+func (i Interactor) GetHistories(r GetHistoriesRequest) ([]domain.History, error) {
+	// all the business logic happens here ... maybe this case is a bit too simple...
+	return r.findHistories(i)
+}
+
+// use privates methods in order not to polute the namespace,
+// one private method by interactor call so its easy to test
+func (r GetHistoriesRequest) findHistories(i Interactor) ([]domain.History, error) {
 	hh, err := i.HistoryRW.FindHistories()
 	if err != nil {
 		return nil, err
 	}
+
+	// I don't know if it's really the expected behavior, it's more for testing fun
+	if hh == nil || len(hh) == 0 {
+		return nil, errors.New("nothing found")
+	} else {
+		processed := []int32{}
+		for _, h := range hh {
+			if h.ID == 0 {
+				return nil, errors.New("a record with no ID has been returned")
+			}
+			for _, id := range processed {
+				if id == h.ID {
+					return nil, errors.New("duplicate records have been returned")
+				}
+			}
+
+			processed = append(processed, h.ID)
+		}
+	}
+
 	return hh, nil
 }
