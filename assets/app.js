7de9c0db8e920208f03c484d5979eecf573291b9
Depado <paul.lhussiez@gmail.com>
2017-11-15 00:19:05 +0100

    Fixing responsive and adding small background

--- assets/app.js
+++ assets/app.js
@@ -1,36 +1,29 @@
-// window.addEventListener("load", function(event) {
-
-var mdlLoaded = setInterval(function(){ 
-    if(typeof document.querySelector('.mdl-js-layout') !== 'undefined') init()
-}, 100)
-
-function init () { 
-    clearInterval(mdlLoaded)
-}
-    var vm = new Vue({
-        el: "#app",
-        data: {
-            search: "",
-            results: [],
-        },
-        delimiters: ['${', '}'],
-        methods: {
-            find: function(event) {
-                if (this.search.length < 8) { 
-                    document.querySelector('.mdl-js-snackbar').MaterialSnackbar.showSnackbar({message: "Invalid siren or siret"}); 
-                    return
-                }
-                var siret = this.search.length > 9;
-                var ep = this.$resource('/api/v1/siren/{id}');
-                if (siret) {
-                    ep = this.$resource('/api/v1/siret/{id}');
-                }
-                ep.get({id: this.search, limit: 100}).then(function(response) {
-                    this.results = siret?[response.data]:response.data;
-                }, function(response) {
-                    document.querySelector('.mdl-js-snackbar').MaterialSnackbar.showSnackbar({message: "This "+ (siret?"siret":"siren") + " doesn't exist"});
-                });
+var vm = new Vue({
+    el: "#app",
+    data: {
+        search: "",
+        results: [],
+    },
+    delimiters: ['${', '}'],
+    methods: {
+        find: function(event) {
+            // Check if valid siren or siret before actually making the request
+            if (this.search.length < 9) {
+                document.querySelector('.mdl-js-snackbar').MaterialSnackbar.showSnackbar({message: "Invalid siren : A siren must be 9 digits"}); 
+                return
             }
+            if ((this.search.length > 9 && this.search.length < 14) || this.search.length > 14) { 
+                document.querySelector('.mdl-js-snackbar').MaterialSnackbar.showSnackbar({message: "Invalid siret : A siret must be 14 digits"}); 
+                return
+            }
+            var siret = this.search.length > 9;
+            var ep = this.$resource('/api/v1/'+(siret?'siret':'siren')+'/{id}');
+            ep.get({id: this.search, limit: 100}).then(function(response) {
+                this.results = siret?[response.data]:response.data;
+            }, function(response) {
+                console.log(response);
+                document.querySelector('.mdl-js-snackbar').MaterialSnackbar.showSnackbar({message: "This "+ (siret?"siret":"siren") + " doesn't exist"});
+            });
         }
-    });
-// });
+    }
+});
